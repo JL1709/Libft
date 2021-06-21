4179996c0eee90a700ebe45a08086ad0111945f0
JL1709 <julian.lu@web.de>
2021-06-21 17:03:30 +0200

    21.06.2021 - 17.03 Uhr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 15:11:51 by julian            #+#    #+#             */
-/*   Updated: 2021/06/21 16:10:33 by julian           ###   ########.fr       */
+/*   Updated: 2021/06/21 17:02:52 by julian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void    *ft_calloc(size_t nmemb, size_t size)
 {
+    void    *ptr;
     
+    if (nmemb == 0 || size == 0)
+        return (NULL)
+    ptr = malloc(nmemb * size);
+    return (ptr);
 }
